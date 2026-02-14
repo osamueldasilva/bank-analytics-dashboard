40e168b733a2af8a0ce36139e739c9e82fec821f
Samuel Barbosa da Silva <103833450+osamueldasilva@users.noreply.github.com>
2026-02-14 14:12:12 -0300

    feat: Initial commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+export default function DashboardPage() {
+      return (
+            <main>
+                  <h1>Dashboard</h1>
+                  <p>Bem-vindo à página do dashboard!</p>
+            </main>
+      );
+}
